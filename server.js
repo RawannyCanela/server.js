de419ce900c6109f52473135421e4b31a581dc8b
RawannyCanela <rawanny@edu.unifil.br>
2024-08-09 20:45:14 -0300

    commit

--- server.js
+++ server.js
@@ -17,11 +17,12 @@ app.get('/consulta-cep/:cep', async (req, res) =>{
         // Fazendo a requisição para ver a API do ViaCEP
         if (!cepRegex.test(cep)) {
             res.status(400).send('CEP inválido')
+            return;
         }
         const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
         res.json(response.data);// Retorna os dados da resposta
     }catch (error) {
-        console.error(`Erro ao fazer requisição:`, error);
+        console.log(`Erro ao fazer requisição:`, error);
         res.status(500).send(`Erro ao consultar o CEP`);
     }  
 });
